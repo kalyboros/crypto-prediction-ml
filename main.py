b8d5c56b7a37861806c1ad6c74ea57df9c86f44b
kalyb <dadosa123@gmail.com>
2021-07-24 23:03:19 +0200

    Running ML for 60 days span with 25 epochs with a real data model

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ start = dt.datetime(2016,1,1)
 end = dt.datetime.now()
 
 data = web.DataReader(f'{crypto_currency}-{pair_currency}', 'yahoo', start, end)
-
 #preparing data for ml model, downscaling the model
 #print(data.head)
 
@@ -28,8 +27,8 @@ prediction_days = 60
 x_train, y_train = [], []
 
 for x in range(prediction_days, len(scaled_data)):
-    x_train.append((scaled_data[x-prediction_days:x, 0]))
-    y_train.append((scaled_data[x, 0]))
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
@@ -39,22 +38,51 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
 
 model = Sequential()
 
-model.add(LSTM(unit=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 model.add(Dropout(0.2))
 #preventing overfitting
-model.add(LSTM(unit=50, return_sequences=True))
+model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(unit=50))
+model.add(LSTM(units=50))
 model.add(Dropout(0.2))
-model.Dense(units=1)
+model.add(Dense(units=1))
 
 model.compile(optimizer='adam', loss='mean_squared_error')
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
+#testing model itself
+test_start = dt.datetime(2020,1,1)
+test_end = dt.datetime.now()
 
+test_data = web.DataReader(f'{crypto_currency}-{pair_currency}', 'yahoo', test_start, test_end)
+actual_prices = test_data['Close'].values
 
+total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 
+#model inputs
+model_inputs = total_dataset[len(total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1,1)
+model_inputs = scaler.fit_transform(model_inputs)
 
+x_test = []
+
+for x in range(prediction_days, len(model_inputs)):
+    x_test.append(model_inputs[x-prediction_days:x, 0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+prediction_prices = model.predict(x_test)
+prediction_prices = scaler.inverse_transform(prediction_prices)
+
+plt.plot(actual_prices, color='black', label='Actual prices')
+plt.plot(prediction_prices, color='yellow', label='Prediction prices')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend(loc='upper left')
+plt.show()
+
+#predicting next day
 
 
 
