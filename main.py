07f4e2ae7ef3541c2a59a37dbbfcbfb3ba8ec7aa
kalyb <dadosa123@gmail.com>
2021-07-24 22:38:16 +0200

    Preparing data.

--- main.py
+++ main.py
@@ -17,4 +17,19 @@ end = dt.datetime.now()
 
 data = web.DataReader(f'{crypto_currency}-{pair_currency}', 'yahoo', start, end)
 
+#preparing data for ml model, downscaling the model
+#print(data.head)
 
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+prediction_days = 60
+
+x_train, y_train = [], []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append((scaled_data[x-prediction_days:x, 0]))
+    y_train.append((scaled_data[x, 0]))
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1],1))
