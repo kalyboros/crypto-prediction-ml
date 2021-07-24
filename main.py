875cea026f68c9a795d8496dab25e2e22cf312eb
kalyb <dadosa123@gmail.com>
2021-07-24 22:31:35 +0200

    Imported nec modules, simple reading from yahoo api and setting points.

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import pandas_datareader as web
+import datetime as dt
+
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import Dense, Dropout, LSTM
+from tensorflow.keras.models import Sequential
+
+crypto_currency = 'BTC'
+pair_currency = 'USD'
+
+start = dt.datetime(2016,1,1)
+end = dt.datetime.now()
+
+data = web.DataReader(f'{crypto_currency}-{pair_currency}', 'yahoo', start, end)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
